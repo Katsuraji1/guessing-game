96426dc566320ed8a958d0aa18809109ba02095e
Katsuraji1 <verahovets@gmail.com>
2022-08-14 18:57:27 +0300

    feat:solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+ this.min = min
+ this.max= max
     }
 
     guess() {
-
+        return this.current=Math.ceil((this.min+this.max)/2)
     }
 
     lower() {
-
+        return this.max = this.current
     }
 
     greater() {
-
+        return this.min = this.current
     }
 }
 
